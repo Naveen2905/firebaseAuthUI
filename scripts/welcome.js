7c5ec588b36bb8aa6eddcc3eaafdbb7772965153
Naveen2905 <naveenmalhotra05@gmail.com>
2020-07-18 21:46:06 -0400

    changes to get user's info

--- scripts/welcome.js
+++ scripts/welcome.js
@@ -4,11 +4,27 @@ $(function () {
     var firebase = app_firebase;
     var uid = null
     firebase.auth().onAuthStateChanged(function (user) {
-        if (user) {
+        var user = firebase.auth().currentUser;
+        var name, email, photoUrl, uid, emailVerified;
+
+        if (user != null) {
             uid = user.uid
-            console.log(user);
-            // User is signed in.
-        } else {
+            name = user.displayName;
+            email = user.email;
+            photoUrl = user.photoURL;
+            emailVerified = user.emailVerified;
+            uid = user.uid;  // The user's ID, unique to the Firebase project. Do NOT use
+            // this value to authenticate with your backend server, if
+            // you have one. Use User.getToken() instead.
+            user.providerData.forEach(function (profile) {
+                console.log("Sign-in provider: " + profile.providerId);
+                console.log("  Provider-specific UID: " + profile.uid);
+                console.log("  Name: " + profile.displayName);
+                console.log("  Email: " + profile.email);
+                console.log("  Photo URL: " + profile.photoURL);
+            });
+        }
+        else {
             uid = null
             window.location.replace('index.html')
         }
@@ -16,6 +32,6 @@ $(function () {
     function logOut() {
         firebase.auth().signOut()
     }
-    
+
     mainApp.logOut = logOut
 });
